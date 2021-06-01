cbf698e1b147891a3eb007a15af0bc247538cdf7
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-06-01 20:18:12 +0300

    Created validateDate function

--- js/form.js
+++ js/form.js
@@ -17,6 +17,30 @@ function validatePhone(phone) {
     return re.test(phone);
 }
 
+function validateDate(date) {
+    const daysInMonth = {
+        1: 31,
+        2: 29,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31
+    };
+    const day = Number(date.substring(0, 2));
+    const month = Number(date.substring(3, 5));
+    if (isNaN(day) || isNaN(month) || !(date[2] == '.'
+        || date[2] == ',' || date[2] == '/')
+        || day < 1 || month < 1 || month > 12
+        || day > daysInMonth[month]) return false;
+    return true;
+}
+
 function getBookTableData() {
     const name = document.getElementById('book-table-name').value;
     const email = document.getElementById('book-table-email').value;
@@ -24,5 +48,4 @@ function getBookTableData() {
     const people = document.getElementById('book-table-people').value;
     const date = document.getElementById('book-table-date').value;
     const time = document.getElementById('book-table-time').value;
-    console.log([name, email, phone, people, date, time]);
 }
