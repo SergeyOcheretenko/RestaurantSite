9b71f3831817b96613b1602be78c810f3d6ef814
Tadanasi <71588076+Avaztazia@users.noreply.github.com>
2021-06-01 21:39:38 +0300

    server-side form

---
+++ server-js/server-form.js
@@ -0,0 +1,68 @@
+'use strict';
+const API = require('./server-api.js');
+class form
+{
+    static validation(value, param) {
+        let response = true;
+        let re;
+        switch (param)
+        {
+            case 'name':
+                for (let i = 0; i < value.length; i++) {
+                    if (!isNaN(Number(value[i]))) responce = false;
+                }
+                break;
+            case 'email':
+                re = /\S+@\S+\.\S+/;
+                response = re.test(value);
+                break;
+            case 'phone':
+                re = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+                response = re.test(value);
+                break;
+            case 'date':
+                const daysInMonth = {
+                    1: 31,
+                    2: 29,
+                    3: 31,
+                    4: 30,
+                    5: 31,
+                    6: 30,
+                    7: 31,
+                    8: 31,
+                    9: 30,
+                    10: 31,
+                    11: 30,
+                    12: 31
+                };
+                const day = Number(value.substring(0, 2));
+                const month = Number(value.substring(3, 5));
+                if (isNaN(day) || isNaN(month) || !(value[2] == '.'
+                    || value[2] == ',' || value[2] == '/')
+                    || day < 1 || month < 1 || month > 12
+                    || day > daysInMonth[month]) responce = false;
+                break;
+            case 'time':
+                re = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+                response = re.test(value);
+                break;
+        };
+        return response;
+    };
+    static getBookTableData() {
+        const data = {
+            name: document.getElementById('book-table-name').value,
+            email: document.getElementById('book-table-email').value,
+            phone: document.getElementById('book-table-phone').value,
+            people: document.getElementById('book-table-people').value,
+            date: document.getElementById('book-table-date').value,
+            time: document.getElementById('book-table-time').value
+        };
+        for (const [key, value] of Object.entries(data))
+        {
+            if ( !this.validation(value, key) ) return key;
+        };
+        API.postForm(data, 'book');
+        return null;
+    };
+};
--- server-js/server-prototype.js
+++ server-js/server-prototype.js
@@ -13,29 +13,38 @@ const mimeTypes = {
 const server = http.createServer()
 
 server.on('request', (req, res) => {
-    const parsedUrl = new URL(req.url, host);
-    let pathName = parsedUrl.pathname;
-    let ext = path.extname(pathName);
-    if (pathName !== '/' && pathName[pathName.length - 1] === '/') 
-    {   //if request without '/' re-request 
-        res.writeHead(302, {'Location': pathName.slice(0, -1)});
-        res.end();
-        return;
-    };
-    switch ( pathName )
+    switch ( req.method )
     {
-        default: //it's a landing page so we can answer only with index.html if it's not json-request
-            ext = '.html';
-            pathName = '../index.html';
+        case 'GET':
+            const parsedUrl = new URL(req.url, host);
+            let pathName = parsedUrl.pathname;
+            let ext = path.extname(pathName);
+            if (pathName !== '/' && pathName[pathName.length - 1] === '/') 
+            {   //if request without '/' re-request 
+                res.writeHead(302, {'Location': pathName.slice(0, -1)});
+                res.end();
+                return;
+            };
+            switch ( pathName )
+            {
+                default: //it's a landing page so we can answer only with index.html if it's not json-request
+                    ext = '.html';
+                    pathName = '../index.html';
+                    break;
+                case '/menu':
+                    ext = '.json';
+                    pathName = '../src/items.json';
+                    break;
+            };
+            const filePath = path.join(process.cwd(), '/public', pathName);
+            const file = fs.createReadStream(filePath);
+            file.pipe(res);
             break;
-        case '/menu':
-            ext = '.json';
-            pathName = '../src/items.json';
+        case 'POST':
+            //saving info
             break;
-    };
-    const filePath = path.join(process.cwd(), '/public', pathName);
-    const file = fs.createReadStream(filePath);
-    file.pipe(res);
+    }
+    
 });
 
 server.listen(5000);
