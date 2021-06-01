5dadb8e4ea6466b647203cf5e5fdb4aeba41144f
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-06-01 19:42:21 +0300

    Created validateEmail function

--- js/form.js
+++ js/form.js
@@ -1,10 +1,15 @@
 'use strict';
 
-function nameValidation(name) {
+function validateName(name) {
     for (let i = 0; i < name.length; i++) {
         if (!isNaN(Number(name[i]))) return false;
     }
     return true;
+}
+
+function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
 }
 
 function getBookTableData() {
@@ -15,5 +20,4 @@ function getBookTableData() {
     const date = document.getElementById('book-table-date').value;
     const time = document.getElementById('book-table-time').value;
     console.log([name, email, phone, people, date, time]);
-    console.log(nameValidation(name));
 }
